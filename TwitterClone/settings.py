1cecbbb3179a528e7df08e077817efc56e2fe2b5
Jess <luna8971@gmail.com>
2022-09-06 23:41:22 -0600

    made some change

--- TwitterClone/settings.py
+++ TwitterClone/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'TwitterClone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ddtqusqel5r0da',                      
-        'USER': 'tiktzynqppekem',
-        'PASSWORD': 'e4024d1f52a11b6c103703f79e23a8f2e758c56b836d1a583dc5fe46771eaaf7',
-        'HOST': 'ec2-34-235-198-25.compute-1.amazonaws.com',
+        'NAME': 'd1muh8ja5jno9a',                      
+        'USER': 'rvagxyfcknvzey',
+        'PASSWORD': '704dca76f1c222e181b4ff26f66d2ed6754475dc77ae2b85eb775c04c70d9a0b',
+        'HOST': 'ec2-54-204-241-136.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 cloudinary.config(
-    cloud_name="dcb7s1d02",
-    api_key="674842767227648",
-    api_secret="g1bx8SbWPy7J0iMPm19G-861sEM",
+    cloud_name="dwrc9uc1y",
+    api_key="251948468979445",
+    api_secret="ulylItXauwhq9bBKqMsc2ztjSzo",
 )
